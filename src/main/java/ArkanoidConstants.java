8dd9f59cd06061af8521c0a34a15f7bd7789b56e
Arthur Khasanov <to_post_88@mail.ru>
2024-06-29 20:56:12 +0500

    was fixed the path to levels #2

--- src/main/java/ArkanoidConstants.java
+++ src/main/java/ArkanoidConstants.java
@@ -18,6 +18,5 @@ public interface ArkanoidConstants {
     int MIN_STICK_SIZE = 40;
 
 
-    File levelsXMLFile = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" +
-            File.separator + "resources" + File.separator + "levels.xml");
+    File levelsXMLFile = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "levels.xml");
 }
